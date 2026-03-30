6867a488bc3f8712b404fd8358463fb3a0b66c19
ryanc192 <148500341+ryanc192@users.noreply.github.com>
2026-03-30 10:19:23 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+let browserClient: SupabaseClient | null = null;
 
 export function createBrowserSupabaseClient() {
-  return createClient(
+  if (browserClient) return browserClient;
+
+  browserClient = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+
+  return browserClient;
 }
