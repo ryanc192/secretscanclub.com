d6bd23dd3749c7277f38bbd040d67096a6c87a81
ryanc192 <148500341+ryanc192@users.noreply.github.com>
2026-04-13 16:20:39 -0400

    Update server.ts

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -1,5 +1,11 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-02-24.acacia",
+const secretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!secretKey) {
+  throw new Error("Missing STRIPE_SECRET_KEY");
+}
+
+export const stripe = new Stripe(secretKey, {
+  apiVersion: "2024-06-20",
 });
